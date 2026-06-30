d49d2edcf33bae2cfc4a219c19fd7eb658d8bf5e
Gabriel <carlossantsgb@gmail.com>
2026-06-30 11:09:10 -0300

    remove-catalog-to-new-repo

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import Termos from '@/pages/Termos'
 import Privacidade from '@/pages/Privacidade'
 import Adminterno from '@/pages/Adminterno'
 import GeradorLinks from '@/pages/GeradorLinks'
-import Catalogo from '@/pages/Catalogo'
 
 export default function App() {
   return (
@@ -20,7 +19,6 @@ export default function App() {
         <Route path="/privacidade" element={<Privacidade />} />
         <Route path="/adminterno" element={<Adminterno />} />
         <Route path="/geradordelinksdepagamento" element={<GeradorLinks />} />
-        <Route path="/catalogo" element={<Catalogo />} />
         {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
         <Route path="*" element={<NotFound />} />
       </Routes>
